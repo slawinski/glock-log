afaf79477e435ab55da9e3b4d01e0ce133da5ca1
slawinski <piotr.slawinski1@gmail.com>
2026-03-02 22:19:30 +0100

    fix font

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,6 @@
 module.exports = {
   preset: "jest-expo",
   setupFilesAfterEnv: [
-    "@testing-library/jest-native/extend-expect",
     "<rootDir>/jest/setup.ts",
   ],
   transformIgnorePatterns: [
